b857bc07364345ed2dcd3cb2521a366a632703c8
your name <you@example.com>
2023-05-29 12:27:53 +0900

    id adjust

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '20d91fac89854fbc85798fdc09d7d7b9',
+  rootNotionPageId: '257c4973f6454caf9a5d8a30fb3c0567',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
